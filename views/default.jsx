6d53131fbd51487d6bd1081389413856c7ddab21
Rashonda Edwards <rashondaedwards@aol.com>
2023-04-24 22:05:15 -0700

    add default view

---
+++ views/default.jsx
@@ -0,0 +1,16 @@
+const React = require('react')
+
+const def = (html)=>{
+    return (
+        <html>
+            <head>
+                <title>Title</title>
+            </head>
+            <body>
+                {html.children}
+            </body>
+        </html>
+    )
+}
+
+module.exports = def
