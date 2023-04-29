348cd5ff2d7c8e088f6cc07e135db96bb20a6965
Rashonda Edwards <rashondaedwards@aol.com>
2023-04-29 13:29:56 -0700

    new places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -6,6 +6,7 @@
 // Changed res.send to res.render 
 // Added an array of objects for my favorite restuarants
 const router = require('express').Router()
+const places = require("../models/places.js")
 
 // Part 5 of rest-rants
 router.get("/new", (req,res)=>{
@@ -13,27 +14,54 @@ router.get("/new", (req,res)=>{
 })
 //
 
+// The Index Route
 router.get("/", (req,res)=>{
-    let places = [{
-        name: 'Red Lobster',
-        city: 'Long Beach',
-        state: 'CA',
-        cuisines: 'Seafoods, Steaks, Pastas, Side Dishes',
-        pic: '/images/red-lobster-food.jpg',
-    },{
-        name: 'Benihanas',
-        city: 'Torrance',
-        state: 'CA',
-        cuisines: 'Fried Rice, Steaks, Vegetables, Seafoods',
-        pic: '/images/Benihana-food.jpg',
-    },]
     res.render("places/index", {places})
 })
 
-// Class 5: rest-rant part5, this was supposed to be added b4 part5 of the activity!!
+// Class 5: rest-rant part5, this was supposed to be added b4 part5 of the activity!! The POST /places route
 router.post("/", (req,res)=>{
-    res.send("POST /places stub")
+    let newPlace = { ...req.body }
+    if (newPlace.pic === "") {
+        // Default image if none is provided
+        newPlace.pic = "https://www.citypng.com/public/uploads/preview/hd-illustration-cartoon-burger-restaurant-png-11653268639gydp7epzbe.png"
+    }
+    if (newPlace.city === "") {
+        newPlace.city = "Anytown"
+    }
+    if (newPlace.state === "") {
+        newPlace.state = "USA"
+    }
+    places.push(newPlace)
+    res.redirect("/places")
 })
-
+// Homework: picture will not show...
   
 module.exports = router
+
+
+
+
+// This... 
+
+    // let places = [{
+    //     name: 'Red Lobster',
+    //     city: 'Long Beach',
+    //     state: 'CA',
+    //     cuisines: 'Seafoods, Steaks, Pastas, Side Dishes',
+    //     pic: '/images/red-lobster-food.jpg',
+    // },{
+    //     name: 'Benihanas',
+    //     city: 'Torrance',
+    //     state: 'CA',
+    //     cuisines: 'Fried Rice, Steaks, Vegetables, Seafoods',
+    //     pic: '/images/Benihana-food.jpg',
+    // },]
+
+/* ... used to be inside of this...
+
+router.get("/", (req,res)=>{
+    res.render("places/index", {places})
+})
+
+*/
---
+++ models/places.js
@@ -0,0 +1,13 @@
+module.exports = [{
+    name: 'Red Lobster',
+    city: 'Long Beach',
+    state: 'CA',
+    cuisines: 'Seafoods, Steaks, Pastas, Side Dishes',
+    pic: '/images/red-lobster-food.jpg',
+},{
+    name: 'Benihanas',
+    city: 'Torrance',
+    state: 'CA',
+    cuisines: 'Fried Rice, Steaks, Vegetables, Seafoods',
+    pic: '/images/Benihana-food.jpg',
+},]
