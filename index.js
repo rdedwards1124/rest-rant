f29e432a76fe4835af43da0c54a8043675fa8698
Rashonda Edwards <rashondaedwards@aol.com>
2023-04-30 09:37:12 -0700

    new submit

--- index.js
+++ index.js
@@ -2,21 +2,32 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 
+// // method override
+// const methodOverride = require('method-override')
+
 // Added for part 3
 
 /* ... I guess i add this now? rest-rant part4*/
 // app.set('views', __dirname + '/views')
 /**/
 
+
+
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 //
 // Class 4: rest-rants part 4
 app.use(express.static("public"))
-app.use("/places", require("./controllers/places"))
 
 // part 5: new app.use
 app.use(express.urlencoded({ extended: true }))
+// Update 4/30/23: That(line24) needs to go b4 this(line26)
+app.use("/places", require("./controllers/places"))
+
+
+
+// // part 6: method override?
+// app.use(methodOverride('_method'))
 
 // Changed res.send to res.render and placed "home" from home.jsx
 app.get("/", (req,res)=>{
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,11 +1,19 @@
 const React = require('react')
 const Def = require('../default')
 
-const show = ()=>{
+const show = (data)=>{
     return (
         <Def>
           <main>
-            <h1>Show Page</h1>
+
+            <h1>{ data.place.name }</h1>
+
+            {/* Part Three: Fill Out the Show Page, number 2 and 3 */}
+            <h2>Rating</h2>
+            <p></p>
+            <h2>Decription</h2>
+            <p></p>
+
           </main>
         </Def>
     )
